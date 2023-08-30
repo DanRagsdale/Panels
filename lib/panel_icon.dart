5cd953a4fb06f849f2c48d6c1ea740e573408a18
Daniel Ragsdale <DanJeffRags@gmail.com>
2023-08-30 16:52:19 -0600

    Added selection indicator to panel icons.

--- lib/panel_icon.dart
+++ lib/panel_icon.dart
@@ -9,11 +9,11 @@ class PanelIcon extends StatelessWidget {
 
 	@override
 	Widget build(BuildContext context) {
-		return Container(
+		var note_icon = Container(
 			margin: const EdgeInsets.all(4),
 			padding: const EdgeInsets.all(8),
 			decoration: BoxDecoration(
-				color: panelMenu.mode == LocalSelectionMode.selected ? Colors.white : COLOR_BACKGROUND_HEAVY,
+				color: COLOR_BACKGROUND_HEAVY,
 				border: Border.all(
 					width: 4,
 				),
@@ -30,6 +30,21 @@ class PanelIcon extends StatelessWidget {
 					),
 				],
 			),
+		);
+
+		if (panelMenu.mode == LocalSelectionMode.unselected) {
+			return note_icon;
+		}
+
+		return Stack(
+			children: [
+				Positioned.fill(child: note_icon,),
+				Checkbox(
+					shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+					value: panelMenu.mode == LocalSelectionMode.selected,
+					onChanged: (_) {},
+				),
+			],
 		);
 	}
 }
